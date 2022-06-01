6091150026a30105744913b44219889718c908dc
Jon <80731303+jsweenQM@users.noreply.github.com>
2022-06-01 16:10:09 -0700

    Update Products.swift

--- Sources/QuinnMurrayLib/Models/Products.swift
+++ Sources/QuinnMurrayLib/Models/Products.swift
@@ -15,17 +15,17 @@ public struct Product: Codable {
     let id: String
     let price: Double
     let inStock, lowStock, inStorePickup: Bool
-    let title, welcomeDescription, imgURL: String
-    let category: Category
-    let unit: Unit
-    let discount: Int
-    let rating: Int?
-    let imgGroup: [String]
+//    let title, welcomeDescription, imgURL: String
+//    let category: Category
+//    let unit: Unit
+//    let discount: Int
+//    let rating: Int?
+//    let imgGroup: [String]
     
     enum CodingKeys: String, CodingKey {
         case id, price, inStock, lowStock, inStorePickup, title
-        case welcomeDescription = "description"
-        case imgURL = "imgUrl"
-        case category, unit, discount, rating, imgGroup
+//        case welcomeDescription = "description"
+//        case imgURL = "imgUrl"
+//        case category, unit, discount, rating, imgGroup
     }
 }
