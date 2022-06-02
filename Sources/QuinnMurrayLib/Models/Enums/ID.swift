1ea74a6af6d851017d94734cff3c81c27000c39c
Jon <80731303+jsweenQM@users.noreply.github.com>
2022-06-02 10:51:56 -0700

    cleanup

---
+++ Sources/QuinnMurrayLib/Models/Enums/ID.swift
@@ -0,0 +1,36 @@
+//
+//  ID.swift
+//  
+//
+//  Created by Jon Sweeney on 6/2/22.
+//
+
+import Foundation
+
+enum ID: Codable {
+    case integer(Int)
+    case string(String)
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let x = try? container.decode(Int.self) {
+            self = .integer(x)
+            return
+        }
+        if let x = try? container.decode(String.self) {
+            self = .string(x)
+            return
+        }
+        throw DecodingError.typeMismatch(ID.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for ID"))
+    }
+
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        switch self {
+        case .integer(let x):
+            try container.encode(x)
+        case .string(let x):
+            try container.encode(x)
+        }
+    }
+}
--- Sources/QuinnMurrayLib/Models/Products.swift
+++ Sources/QuinnMurrayLib/Models/Products.swift
@@ -9,29 +9,6 @@ import Foundation
 
 typealias Product = [ProductElement]
 
-//public struct ProductsResponse: Codable {
-//    let products: [Product]
-//}
-//
-//public struct Product: Codable {
-////    let id: String
-//    let price: Double
-////    let inStock, lowStock, inStorePickup: Bool
-////    let title, welcomeDescription, imgURL: String
-////    let category: Category
-////    let unit: Unit
-////    let discount: Int
-////    let rating: Int?
-////    let imgGroup: [String]
-//    
-//    enum CodingKeys: String, CodingKey {
-//        case price
-////        case id, price, inStock, lowStock, inStorePickup//, title
-////        case welcomeDescription = "description"
-////        case imgURL = "imgUrl"
-////        case category, unit, discount, rating, imgGroup
-//    }
-//}
 public struct ProductElement: Codable {
     let id: ID
     let price: Double
@@ -50,51 +27,3 @@ public struct ProductElement: Codable {
         case category, unit, discount, rating, imgGroup
     }
 }
-
-enum Category2: String, Codable {
-    case automotive = "automotive"
-    case bikes = "bikes"
-    case electronics = "electronics"
-    case fashion = "fashion"
-    case furniture = "furniture"
-    case giftshop = "giftshop"
-    case groceries = "groceries"
-    case grocery1 = "grocery1"
-    case healthBeauty = "health&beauty"
-    case healthbeauty = "healthbeauty"
-    case homeGarden = "home&garden"
-    case music = "music"
-}
-
-enum ID: Codable {
-    case integer(Int)
-    case string(String)
-
-    init(from decoder: Decoder) throws {
-        let container = try decoder.singleValueContainer()
-        if let x = try? container.decode(Int.self) {
-            self = .integer(x)
-            return
-        }
-        if let x = try? container.decode(String.self) {
-            self = .string(x)
-            return
-        }
-        throw DecodingError.typeMismatch(ID.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for ID"))
-    }
-
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.singleValueContainer()
-        switch self {
-        case .integer(let x):
-            try container.encode(x)
-        case .string(let x):
-            try container.encode(x)
-        }
-    }
-}
-
-enum Unit2: String, Codable {
-    case kg = "kg"
-}
-
