5451a21fb9098f37b902a0946e90e29616628318
Jon <80731303+jsweenQM@users.noreply.github.com>
2022-06-07 08:23:40 -0700

    Update Products.swift

--- Sources/QuinnMurrayLib/Models/Products.swift
+++ Sources/QuinnMurrayLib/Models/Products.swift
@@ -11,14 +11,14 @@ typealias Product = [ProductElement]
 
 public struct ProductElement: Codable {
     public let id: ID
-    let price: Double
-    let inStock, lowStock, inStorePickup: Bool
-    let title, productDescription, imgURL: String
-    let category: Category
-    let unit: Unit
-    let discount: Int
-    let rating: Int?
-    let imgGroup: [String]
+    public let price: Double
+    public let inStock, lowStock, inStorePickup: Bool
+    public let title, productDescription, imgURL: String
+    public let category: Category
+    public let unit: Unit
+    public let discount: Int
+    public let rating: Int?
+    public let imgGroup: [String]
 
     enum CodingKeys: String, CodingKey {
         case id, price, inStock, lowStock, inStorePickup, title
