9d7b57b132ae52aa5a816fb7438c81bc9304569e
Jon <80731303+jsweenQM@users.noreply.github.com>
2022-06-08 08:33:09 -0700

    define bundle path

---
+++ Sources/QuinnMurrayLib/Resources/File.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jon Sweeney on 6/8/22.
+//
+
+import Foundation
+
+#if canImport(UIKit)
+import UIKit
+
+public let favouriteIcon = UIImage(named: "QMLogoPink", in: Bundle.module, compatibleWith: nil)
+
+#endif
