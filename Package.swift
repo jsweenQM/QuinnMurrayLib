5341b42b90df25cf605bf18a8a05d1bf1ed7d025
Jon <80731303+jsweenQM@users.noreply.github.com>
2022-06-08 08:24:31 -0700

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "QuinnMurrayLib",
-            dependencies: []),
+            dependencies: [],
+            resources: [
+                .process("Resources")]),
         .testTarget(
             name: "QuinnMurrayLibTests",
             dependencies: ["QuinnMurrayLib"]),
